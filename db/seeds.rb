adb1a81a92cdac2347ad9552cd2879359eaebf4b
Ferdie Quizon <fquizon@gmail.com>
2015-11-02 18:32:38 -0800

    created user profile list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# clean out all current data
+User.delete_all
+
+ferdie = User.create(
+  name: "Ferdie Q",
+  username: "ferdiesbites",
+  email: "ferdie@email.com",
+  gender: "M",
+  city: "Los Angeles",
+  country: "USA",
+  moderator: true,
+  password:              "password",
+  password_confirmation: "password"
+)
+
+garfield = User.create(
+  name: "Garfield",
+  username: "ILOVELASAGNA",
+  email: "garfield@email.com",
+  gender: "M",
+  city: "Suburbia",
+  country: "USA",
+  password:              "123",
+  password_confirmation: "123"
+)
+
+homer = User.create(
+  name: "Homer Simpson",
+  username: "MAX POWER",
+  email: "homer.j.simpson@email.com",
+  gender: "M",
+  city: "Springfield",
+  country: "USA",
+  password:              "123",
+  password_confirmation: "123"
+)
+
+patrick = User.create(
+  name: "Patrick Star",
+  username: "Barnicle Boy",
+  email: "patrick@email.com",
+  gender: "M",
+  city: "Bikini Bottom",
+  country: "USA",
+  moderator: false,
+  password:              "123",
+  password_confirmation: "123"
+)
+
+domo = User.create(
+  name: "Domo",
+  username: "Nom-Nom ",
+  email: "domo@email.com",
+  gender: "M",
+  city: "Tokyo",
+  country: "Japan",
+  moderator: false,
+  password:              "123",
+  password_confirmation: "123"
+)
