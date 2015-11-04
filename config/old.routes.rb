9867166b224a625dfd4b601c8d717d04b2d8c570
Ferdie Quizon <fquizon@gmail.com>
2015-11-03 17:54:00 -0800

    Added Bootstrap

---
+++ config/old.routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root "users#index"
+  get 'welcome' => 'pages#home'
+
+  get '/comments' => 'comments#index'
+  get '/comments/new' => 'comments#new'
+  post 'comments' => 'comments#create'
+
+
+
+  resources :users, only: [:index, :new, :create, :destroy]
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+end
